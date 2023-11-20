6429fb8e34157ed2b6f9d23ab85142bbee0724d4
Jayanth Karthik <jayanthkarthik50@gmail.com>
2023-11-19 18:49:42 -0600

    setup navigation bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import logo from "../assets/logo.png";
+import styles from "../styles/Navbar.module.css";
+
+export default function NavBar() {
+    return (
+        <AppBar position="static" className={styles.main}>
+            <Toolbar>
+                <Stack width="50%" alignItems="center" direction="row" spacing={3}>
+                    <a href="/" >
+                        <img src={logo} width={40}></img>
+                    </a>
+                    <Typography className={styles.content} sx={{ flexGrow: 1 }}>
+                        <h3> Game Day Grill </h3>
+                    </Typography>
+                </Stack>
+                <Stack
+                    width="50%"
+                    justifyContent="flex-end"
+                    direction="row"
+                    spacing={2}
+                >
+                    <Button className={styles.content} href="/"> Home </Button>
+                    <Button className={styles.content} href="/menu">Menu</Button>
+                    <Button className={styles.content} href="/contact">Contact</Button>
+                    <Button className={styles.content} href="/reservation"> Reservation</Button >
+
+                </Stack >
+            </Toolbar >
+        </AppBar >
+    );
+}
