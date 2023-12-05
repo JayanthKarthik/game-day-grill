1ea6391503dd1af816ade80455ce74e30d628962
Jayanth Karthik <jayanthkarthik50@gmail.com>
2023-12-04 18:41:00 -0600

    idk man i just commited for fun

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,3 +1,18 @@
-export default function AboutUs() {
-    return <h1>AboutUs</h1>
+import { Divider, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import styles from "../styles/Menu.module.css";
+export default function Menu() {
+
+    return (
+        <div>
+            <Stack direction="column" alignItems="center">
+                <h1 className={styles.title}>
+                    hi
+                </h1>
+                <p1 className={styles.text}>
+                    stuff
+                </p1>
+            </Stack>
+        </div >
+    )
 }
