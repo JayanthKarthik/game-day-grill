b81e9cc31feaa3a9fa6dfe2fb66ce9768de07dfa
Jayanth Karthik <jayanthkarthik50@gmail.com>
2023-12-10 20:51:20 -0600

    finished everything on the frontend except fro reservation, and i need to fix the prices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,37 @@
 import styles from "../styles/AboutUs.module.css";
+import { Stack } from "@mui/system";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import style from "../styles/Navbar.module.css";
 
 export default function home() {
     return (
         <div>
-            <div className={styles.backgroundImg}>
-                <div className={styles.backgroundContent}>
-                    <h1 className={styles.headingPrimary}>
-                        <span>welcome</span> to game day grill
+            <Stack>
+                <div className={styles.backgroundImg}>
+                    <div className={styles.backgroundContent}>
+                        <h1 className={styles.headingPrimary}>
+                            <span>welcome</span> to game day grill
                     </h1>
-
+                    </div>
+                    <AppBar position="static" className={styles.main}>
+                        <Toolbar>
+                            <Stack
+                                width="50%"
+                                justifyContent="flex-end"
+                                direction="row"
+                                spacing={2}
+                            >
+                                <Button className={style.content} href="/"> Home </Button>
+                                <Button className={style.content} href="/menu">Menu</Button>
+                                <Button className={style.content} href="/contact">Contact</Button>
+                                <Button className={style.content} href="/reservation"> Reservation</Button >
+                                <Button className={style.content} href="/about"> About</Button >
+                            </Stack >
+                        </Toolbar >
+                    </AppBar >
                 </div>
-            </div>
+            </Stack>
         </div >
+
     )
 }
