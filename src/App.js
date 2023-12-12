d3cb729ca5b20355af932f6218f08b339d92b14b
Jayanth Karthik <jayanthkarthik50@gmail.com>
2023-12-11 22:16:20 -0600

    more fixez

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Menu from './pages/Menu'
 import Reservation from './pages/Reservation'
+import About from './pages/About'
+
 
 export default function App() {
   return (
@@ -15,7 +18,9 @@ export default function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/reservation" element={<Reservation />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
 
     </body>
   );
